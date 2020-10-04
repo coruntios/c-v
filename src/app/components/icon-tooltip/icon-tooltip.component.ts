b47e2d4edb6d101c3ad289392051d63b84c66e70
Marcelo <majg8a@gmail.com>
2020-10-03 22:12:18 -0400

    quality of life

--- src/app/components/icon-tooltip/icon-tooltip.component.ts
+++ src/app/components/icon-tooltip/icon-tooltip.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 export class IconTooltipComponent implements OnInit {
   @Input() title = "";
   @Input() image = "";
+  @Input() url = "";
+
 
   visible = false;
   constructor() { }
